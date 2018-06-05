7ab3b6945ac03ef2be65bf99b4896173d2bf390f
Michael Gailey <mbgailey@gmail.com>
2018-06-05 11:59:54 +0100

    Initial commit

---
+++ Assets/GUI/MainMenuUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public void StartGame()
+  {
+    SceneManager.LoadScene(1);
+  }
+
+  public void QuitApplication()
+  {
+    Application.Quit();
+  }
+}
